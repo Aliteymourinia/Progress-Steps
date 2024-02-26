de3c1afaf5c50a7159af8523c60b2a2aff31a871
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-02-26 20:51:12 +0330

    Code cleanup

--- script.js
+++ script.js
@@ -3,32 +3,46 @@ const next = document.querySelector("#next");
 const prev = document.querySelector("#prev");
 const circles = document.querySelectorAll(".circle");
 let currentActive = 0;
-const pixelValues = [, 260, 700, 1060];
-next.addEventListener("click", () => {
-  currentActive++;
-  prev.disabled = false;
-  prev.style.border = "#3B83F6";
-  prev.style.borderStyle = "#3B83F6";
+const pixelValues = [20, 260, 580, 850];
+
+const updateProgress = () => {
   progress.style.width = pixelValues[currentActive] + "px";
   circles.forEach((circle, index) => {
-    if (index === currentActive) {
-      circle.style.borderColor = "#3B83F6";
-    }
+    circle.style.borderColor = index <= currentActive ? "#3B83F6" : "";
   });
+};
+const enablePrev = () => {
+  prev.disabled = false;
+  prev.style.border = prev.style.borderStyle = "#3B83F6";
+};
+
+const disablePrev = () => {
+  prev.disabled = true;
+  prev.style.border = prev.style.borderStyle = "";
+};
+next.addEventListener("click", () => {
+  currentActive++;
+  enablePrev();
+  updateProgress();
 });
 prev.addEventListener("click", () => {
   currentActive--;
-  if (currentActive > 0) {
-    prev.classList.add("active:scale-95");
-    progress.style.width = pixelValues[currentActive] + "px";
-  } else {
-    prev.disabled = true;
-    progress.style.width = "";
-    prev.classList.remove("active:scale-95");
-  }
-  circles.forEach((circle, index) => {
-    if (index > currentActive) {
-      circle.style.borderColor = "";
-    }
-  });
+  index > currentActive ? enablePrev() : "";
+  updateProgress();
+  disablePrev();
 });
+// prev.addEventListener("click", () => {
+//   currentActive--;
+//   if (currentActive > 0) {
+//     prev.classList.add("active:scale-95");
+//     progress.style.width = pixelValues[currentActive] + "px";
+//   } else {
+//     prev.disabled = true;
+//     progress.style.width = "";
+//     prev.classList.remove("active:scale-95");
+//   }
+//   circles.forEach((circle, index) => {
+//     if (index > currentActive) {
+//       circle.style.borderColor = "";
+//     }
+//   });
